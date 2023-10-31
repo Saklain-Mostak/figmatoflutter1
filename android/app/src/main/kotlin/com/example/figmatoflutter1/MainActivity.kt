6a5e9e19fb372e6f19af74e18476bb2a4b6787f2
saklain <saklain.pub@gmail.com>
2023-10-31 21:25:47 +0600

    figmatoflutter1

---
+++ android/app/src/main/kotlin/com/example/figmatoflutter1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.figmatoflutter1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:figmatoflutter1/homescreen/firstpage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Figma to Flutter 1',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FigmaToFlutter1(),
+    );
+  }
+}
+
